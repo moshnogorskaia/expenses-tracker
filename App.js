b239128d36aa38208f0bbe3394ea93024ade0d5e
Marina Moshnogorskaia <marina.moshnogorskaia@erasys.de>
2025-03-18 14:32:35 +0100

    Navigate to Add and Edit expense modal

--- App.js
+++ App.js
@@ -63,7 +63,12 @@ export default function App() {
 		<>
 			<StatusBar style="auto" />
 			<NavigationContainer>
-				<Stack.Navigator>
+				<Stack.Navigator
+					screenOptions={{
+						headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+						headerTintColor: "white",
+					}}
+				>
 					<Stack.Screen
 						name="ExpensesOverview"
 						component={ExpensesOverview}
@@ -71,7 +76,13 @@ export default function App() {
 							headerShown: false,
 						}}
 					/>
-					<Stack.Screen name="ManageExpense" component={ManageExpense} />
+					<Stack.Screen
+						name="ManageExpense"
+						component={ManageExpense}
+						options={{
+							presentation: "modal",
+						}}
+					/>
 				</Stack.Navigator>
 			</NavigationContainer>
 		</>
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,7 +1,19 @@
-import { Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
+import { useLayoutEffect } from "react";
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+	const editedExpenseId = route.params?.expenseId;
+	const isEditing = Boolean(editedExpenseId);
+
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			title: isEditing ? "Edit Expense" : "Add Expense",
+		});
+	}, [isEditing, navigation]);
+
 	return <Text>ManageExpense</Text>;
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({});
