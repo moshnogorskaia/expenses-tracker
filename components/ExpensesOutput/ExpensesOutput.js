37787f3fabd6e0ad754cbae0e70de75f347ca413
Marina Moshnogorskaia <marina.moshnogorskaia@erasys.de>
2025-03-18 10:37:12 +0100

    Add dummy data

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,11 +1,38 @@
 import { View } from "react-native";
-import ExpensesSummary from "../ExpensesSummary/ExpensesSummary";
-import ExpensesList from "../ExpensesList/ExpensesList";
+import ExpensesSummary from "./ExpensesSummary";
+import ExpensesList from "./ExpensesList";
+
+const DUMMY_EXPENSES = [
+	{
+		id: "e1",
+		title: "New Shoes",
+		amount: 69.99,
+		date: new Date(2021, 7, 14),
+	},
+	{
+		id: "e2",
+		title: "Weekly Groceries",
+		amount: 100.0,
+		date: new Date(2021, 7, 15),
+	},
+	{
+		id: "e3",
+		title: "New TV",
+		amount: 799.49,
+		date: new Date(2021, 7, 16),
+	},
+	{
+		id: "e4",
+		title: "Car Insurance",
+		amount: 294.67,
+		date: new Date(2021, 7, 17),
+	},
+];
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
 	return (
 		<View>
-			<ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+			<ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
 			<ExpensesList />
 		</View>
 	);
