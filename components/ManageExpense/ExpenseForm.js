680f17ba784c83a4a0bc563302b12d1df124304b
Marina Moshnogorskaia <marina.moshnogorskaia@erasys.de>
2025-03-18 16:33:46 +0100

    Configure form inputs

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,8 +1,38 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import Input from "./Input";
 
 function ExpenseForm() {
-	return <View></View>;
+	function amountChangeHandler() {}
+
+	return (
+		<View>
+			<Input
+				label="amount"
+				textInputConfig={{
+					keyboardType: "decimal-pad",
+					placeholder: "0.00",
+					onChangeText: amountChangeHandler,
+				}}
+			/>
+			<Input
+				label="date"
+				textInputConfig={{
+					placeholder: "YYYY-MM-DD",
+					maxLength: 10,
+					onChangeText: () => {},
+				}}
+			/>
+			<Input
+				label="description"
+				textInputConfig={{
+					multiline: true,
+					onChangeText: () => {},
+				}}
+			/>
+		</View>
+	);
 }
 
 export default ExpenseForm;
+
+const styles = StyleSheet.create({});
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -4,6 +4,7 @@ import IconButton from "../components/ui/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/ui/Button";
 import { ExpensesContext } from "../store/expenses-context";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
 function ManageExpense({ route, navigation }) {
 	const editedExpenseId = route.params?.expenseId;
@@ -44,6 +45,7 @@ function ManageExpense({ route, navigation }) {
 
 	return (
 		<View style={styles.container}>
+			<ExpenseForm />
 			<View style={styles.buttons}>
 				<Button
 					mode="flat"
